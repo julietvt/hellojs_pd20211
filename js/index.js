1356e98430240958446121bfba5062ac09cfc6f9
julietvt <freshitpt@gmail.com>
2021-08-29 10:53:34 +0300

    work with object

--- js/index.js
+++ js/index.js
@@ -1,235 +1,107 @@
-'use strict';
+/* Обьекты */
+// имя_объекта.имя_свойства
 
-// условие if-else
-// циклы for() , while(i<100), do-while
+const user1 = {};
 
-// конструкция switch
+console.log(user1);
 
+console.log(typeof user1);
+
+const user2 = {
+  firstName: 'Vasya',
+  lastName: 'Ivanov',
+  workExperience: 0,
+  isMale: true,
+};
+
+console.log(user2);
+
+console.log(typeof user2);
+
+console.log(user2.firstName, user2.lastName);
+
+// добавление нового свойства
+user2.category = 'junior';
+console.log(user2);
+
+user2.workExperience = 1; // изменение свойства для обьекта
+console.log(user2);
+
+user2_copy = user2;
+console.log(user2);
+
+const user3 = {
+  firstName: 'Jack',
+  lastName: 'Sparrow',
+  isMale: true,
+  sayHello: function () {
+    console.log('Hello! My name is ', this.firstName, ' ', this.lastName);
+  },
+  doGreeting: function (someUser) {
+    console.log('Hi, ', someUser.firstName, '!!!');
+  },
+  sayHelloUser2: function () {
+    console.log('Hello! User2: ', user2.firstName, ' ', user2.lastName);
+  },
+  outThisObject: function () {
+    console.log(this);
+  },
+};
+
+console.log(user3);
+console.log(user3.sayHello());
+console.log(user3.sayHelloUser2());
+console.log(user3.outThisObject());
+console.log(user3.doGreeting(user2));
+
+const user4 = {
+  0: 'Vasya',
+  1: 'Ivanov',
+  2: 0,
+  3: true,
+};
+console.log(user4);
+// console.log(user4.0); ошибка!
+
+console.log(user4[0]);
+console.log(user4[1]);
+console.log(user4[2]);
+console.log(user4[3]);
+
+//console.log(user3['firstName']);
+
+// удалить свойство обьекта
+delete user3.category;
+console.log(user3);
+
+function mySum(a, b) {
+  return a + b;
+}
+
+function helloWithUser(user) {
+  console.log('Hello user ', user.firstName, user.lastName);
+}
+
+console.log('Funciton helloWithUser');
+helloWithUser(user1);
+helloWithUser(user2);
+helloWithUser(user3);
+
+const car = {
+  model: 'mazda',
+  color: 'red',
+  year: 2020,
+  engine: {
+    volume: 2.0,
+    year: 2019,
+    typet: {
+      name: 'some_name',
+    },
+  },
+};
 /*
-let x = prompt();
-
-if (x == 1) {
-  alert('User have entered 1');
-} else if (x == 2) {
-  alert('User have entered 2');
-} else if (x == 3) {
-  alert('User have entered 3');
-} else if (x == 4) {
-  alert('User have entered 4');
-} else if (x == 5) {
-  alert('User have entered 5');
-} else if (x == 6) {
-  alert('User have entered 6');
-} else if (x == 7) {
-  alert('User have entered 7');
-} else {
-  alert('User have entered some number');
-}
-
-
-switch (x) {
-  case 'A':
-    {alert('User have entered A');
-    break;}
-  case 'B':
-    alert('User have entered B');
-    break;
-  case 'C':
-    alert('User have entered C');
-    break;
-  default: alert('User have entered some character'); break;
-}
-
+car.model
+car.engine
+car.engine.volume
+car.engine.typet.name
 */
-/*
-function myAdd(x1, x2) {
-  return x1 + x2;
-}
-
-function mySub(x1, x2) {
-  return x1 - x2;
-}
-
-function myDiv(x1, x2) {
-  return x1 / x2;
-}
-
-function myMult(x1, x2) {
-  return x1 * x2;
-}
-
-*/
-
-/*
-
-let number1 = prompt('Enter number1');
-let number2 = prompt('Enter number2');
-let op = prompt('Enter sign (+,-,/,*)');
-*/
-/*
-if(op == '+') {myAdd(number1,number2);
-} else if(op == '-'){mySub(number1,number2);
-  } else if(op == '/'){myDiv(number1,number2);
-    } else if(op == '*'){myMult(number1,number2);
-    } else{console.log('Unknown operator');}
-*/
-/*
-switch (op) {
-  case '+': {
-    console.log(myAdd(number1, number2));
-    break;
-  }
-  case '-': {
-    console.log(mySub(number1, number2));
-    break;
-  }
-  case '/': {
-    console.log(myDiv(number1, number2));
-    break;
-  }
-  case '*': {
-    console.log(myMult(number1, number2));
-    break;
-  }
-  default: {
-    console.log('Unknown operator');
-    break;
-  }
-}
-*/
-
-// function declaration
-function myAdd1(x1, x2) {
-  return x1 + x2;
-}
-
-// function expression
-let myAdd2 = function (x1, x2) {
-  return x1 + x2;
-};
-
-console.log(myAdd1(2, 2));
-console.log(myAdd2(2, 2));
-
-//array function
-
-let myAdd3 = (x1, x2) => x1 + x2;
-
-let myAdd4 = (x1, x2) => {
-  let result;
-  result = x1 + x2;
-  return result;
-};
-
-console.log(myAdd3(2, 2));
-console.log(myAdd4(2, 2));
-
-let sayHelloWithUser = (userName) => {
-  let greeting = 'Hello, ' + userName;
-  console.log(greeting);
-};
-
-let mySub2 = (x1, x2) => {
-  let result;
-  result = x1 - x2;
-  return result;
-};
-
-let myMult2 = (x1, x2) => x1 * x2;
-
-let myDiv2 = (x1, x2) => x1 / x2;
-
-// Вывести числа от заданного до нуля
-
-let myFun1 = (userValue) => {
-  // for
-  let i;
-  for (i = userValue; i >= 0; i--) {
-    console.log(i);
-  }
-  /*
-  for(; userValue>=0; userValue--){
-    console.log(userValue);
-  }
-  */
-};
-
-console.log('Task 5-1');
-myFun1(5);
-
-let myFun2 = (userValue) => {
-  // while
-  while (userValue > 0) {
-    console.log(userValue);
-    userValue--;
-  }
-};
-console.log('Task 5-2');
-myFun1(5);
-
-/*
-TASKS
-
-Стрелочные функци
-
-1 Определите в какую четверть часа попадает введенное количество минут 
-пользователем (например, 40 минут - 3-я четверть часа)
-
-2 Найти произведение чисел в пределах от limit1 до limit2
-
-*/
-
-let userVal = (minutes) => {
-  let part = Math.ceil(minutes / 15);
-  return part;
-};
-
-let userVal = prompt('Введите минуты');
-console.log('The part of an hour is ', definePartOfHour(userVal));
-
-/* плохое решение
-
-let userVal = (minutes) => {
-  let part = Math.ceil(minutes / 15);
-  switch (part) {
-    case 1:
-      console.log('1 part of an hour');
-      break;
-    case 2:
-      console.log('2 part of an hour');
-      break;
-    case 3:
-      console.log('3 part of an hour');
-      break;
-    case 4:
-      console.log('4 part of an hour');
-      break;
-    default: console.log('error'); break;
-  }
-};
-
-let userVal = prompt('Введите минуты');
-console.log(definePartOfHour(userVal));
-
-*/
-
-let multWithLimits = (limit1, limit2) => {
-  if (limit1 > limit2) {
-    let buf = limit1;
-    limit1 = limit2;
-    limit2 = buf;
-  }
-  let i,
-    result = 1;
-  for (i = limit1; i <= limit2; i++) {
-    result *= i;
-  }
-  /*
-  for (; limit1 <= limit2; limit1++) {
-    result *= limit1;
-  }
-  */
-  return result;
-};
-
-console.log(multWithLimits(2, 5));
