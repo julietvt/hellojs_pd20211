3d3d7adc4dc13f701f65bdebb38b08f7791048b7
julietvt <freshitpt@gmail.com>
2021-09-05 10:54:34 +0300

    область видимости и лексическое окружениеЭ

--- js/index.js
+++ js/index.js
@@ -1,63 +1,225 @@
-// СТРОКИ String
-
-let str1 = 'hello world!!!';
-let str2 = '';
-console.log(str1);
-
-// доступ str[0], str[1]
-const [firstCh, ...restStr] = str1;
-
-console.log(str1.charAt(0));
+// область видимости
 
 /*
-\\ обратный слеш
-\t табуляция
-\n новая строка
-\` кавычки \"
-\u юникод таблица
+let userValue = prompt('userValue');
+
+const test = 100;
+
+switch (userValue) {
+  case '1':
+    {
+      console.log(test);
+    }
+    break;
+  case '2':
+    {
+      console.log(test);
+    }
+    break;
+  case '3':
+    {
+      console.log(test);
+    }
+    break;
+  case '4':
+    {
+      console.log(test);
+    }
+    break;
+  default:
+    {
+      console.log(test);
+    }
+    break;
+}
+*/
+
+/*
+const test1 = 200;
+if (true) {
+  const test2 = 100;
+  console.log('First out test1=', test1);
+  console.log('First out test2=', test2);
+}
+console.log('Second out test1=', test1);
+console.log('Second out test2=', test2);
+*/
+
+/*
+const test = 200;
+if (true) {
+  const test = 100;
+  console.log('First out', test);
+}
+console.log('Second out', test);
+*/
+
+/*
+const test = 10;
+if (true) {
+  console.log(test);
+}
+*/
+
+/*
+if (true) {
+  var test = 1000000;
+  console.log(test);
+}
+console.log(test);
+*/
+
+/*
+console.log(test);
+
+if (true) {
+  console.log(test);
+  if (true) {
+    test = 1;
+    console.log(test);
+    if (true) {
+      console.log(test);
+      if (true) {
+        console.log(test); // undefined
+        var test;
+        test = 1000000;
+        console.log(test); //1000000
+      }
+      console.log(test);
+    }
+    console.log(test);
+  }
+  console.log(test);
+}
+console.log(test);
+/*
+console.log(test2);
+let test2 = 5;
+console.log(test2);
+*/
+
+//console.log(this);
+
+'use strict';
+
+/*
+function f() {
+  test = 10;
+}
+
+f();
+console.log(this);
+*/
+
+/*
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log('Result i=', i);
+*/
+/*
+const test = 10;
+
+function myFunction() {
+  console.log(test);
+  return 0;
+}
+
+const result = myFunction();
+console.log(result);
+*/
+
+/*
+function f() {
+  return function () {
+    //console.log('Hello');
+    alert('hello');
+  };
+}
+
+const result2 = f();
+result2();
+*/
+
+/*
+function f() {
+  const x = 10;
+  return function () {
+    return x;
+  };
+}
+
+//const result = f();
+//result();
+
+const resultFunction = f();
+const result = resultFunction();
+console.log(result);
+*/
+
+/*
+function f() {
+  let x = 10;
+  return function () {
+    return x++;
+  };
+}
+
+const resultFunction = f();
+console.log('resultFunction 1');
+console.log(resultFunction());
+console.log(resultFunction());
+console.log(resultFunction());
+console.log(resultFunction());
+console.log(resultFunction());
+
+const resultFunction2 = f();
+console.log('resultFunction 2');
+console.log(resultFunction2());
+console.log(resultFunction2());
+console.log(resultFunction2());
+console.log(resultFunction2());
+console.log(resultFunction2());
+*/
+
+/*
+function f() {
+  let x = 10;
+  function test() {}
+  return function () {
+    test();
+    x++;
+  };
+}
+*/
+
+/*
+function f() {
+  var x = 0;
+}
+if (false) {
+  var x = 0;
+}
+f();
+
+// каждый раз при вызове функции создается новое лексическое окружение
+
+const test = 10;
+function f() {
+  if (true) {
+    var value = test;
+  }
+}
+
+f();
 
 */
 
-console.log('word "hello"');
-//console.log('word \'hello\'');
-console.log('word \nhello \nhi');
-console.log('word \thello \thi');
-
-// \ экранирование
-console.log('\u{263A}');
-console.log('\u{1F608}');
-
-// длина строки
-console.log(str1.length);
-
-//перевод строки с массив
-str1.toCharArray();
-
-// задачи на строки
-//1 строку преобразовать в массив слов
-
-let str = '   hello world  string  John  Fox';
-
-function task1(str) {
-  return str.trim().split(' ');
+function f(userValue) {
+  ++userValue;
+  console.log(userValue);
+  return userValue;
 }
 
-// сделать первую букву заглавной
-str[0].toUpperCase() + str.slice(1);
-// сделать каждое слово с заглавной буквы
-
-//сделать переменную в верблюжем регистре
-let words = str.split(' ');
-
-// вставить слово в предложении в указанную позицию
-
-function task10(str, word, position) {
-  let words = str.split('');
-  words.splice(position, 0, word);
-  let resultStr = words.join('');
-  return resultStr;
-}
-
-// найти сколько раз какойто символ встречается в строке
-
-function task11(str, searchSymbol) {}
+let userValue = 5;
+console.log(f(userValue));
