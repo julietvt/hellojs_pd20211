4b403b9ac6a410c2843ebcbefd3fe146347a9285
julietvt <freshitpt@gmail.com>
2021-09-05 12:35:50 +0300

    работа с обьектами

--- js/index.js
+++ js/index.js
@@ -1,78 +1,106 @@
-// рекурсия
+// заморозка обьекта
+const READ = 'READ';
+const user = Object.freeze({
+  name: 'John',
+  surname: 'Fox',
+  age: 20,
+  isMale: true,
+  role: READ,
+});
 
-// число в степени
-// base ^ exp  2^5=2*2*2*2*2
+// назначение прав доступа
+
+// admin, moderator, user
+const ROLE = Object.freeze({
+  ADMIN: 'ADMIN',
+  MODERATOR: 'MODERATOR',
+  USER: 'USER',
+});
+// create, read, update, delete
+const ACTION = {
+  CREATE: 'CREATE',
+  READ: 'READ',
+  UPDATE: 'UPDATE',
+  DELETE: 'DELETE',
+};
+
+// role rights
+
+let rights = new Map();
+
+rights.set(ACTION.CREATE, [ROLE.ADMIN, ROLE.MODERATOR]);
+rights.set(ACTION.READ, [ROLE.USER, ROLE.MODERATOR, ROLE.ADMIN]);
+rights.set(ACTION.UPDATE, [ROLE.MODERATOR]);
+rights.set(ACTION.DELETE, [ROLE.ADMIN]);
+
+function checkPermission(action, role) {
+  if (rights.has(action)) {
+    return rights.get(action).includes(role);
+  }
+  return false;
+}
+
+//rights.set(ROLE.USER, [ACTION.READ]);
 
 /*
-function pow1 (base, exp){
-  for(let i=2; i<=exp; i++){
-    base *= base;
-  }
-  return base;
+const user1 = {
+  login: 'user1',
+  role_access: ROLE.USER,
+  action_access: {ACTION.READ}  
 }
 */
 
-/*
-function pow1(base, exp) {
-  let result = 1;
-  for (let i = 1; i <= exp; i++) {
-    result = result * base;
-  }
-  return result;
-}
+// деструктуризация
 
-console.log(pow1(2, 5));
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+// el1 = 1, el2 = 2, rest = [3,4,5,6,7,8,10]
+const [el1, el2, ...rest] = arr;
+
+const arr2 = [1, 2, [3, 4, 5, 6], 7, 8, 10];
+//5
+const [, , [, , el5]] = arr2;
+
+// object
+
+const user100 = {
+  name: 'Tom',
+  surname: 'Fox',
+  age: 20,
+  book: {
+    cover: {
+      pages: 200,
+      format: '20x45',
+      copies: 5000,
+    },
+    bookName: 'java script',
+    bookYear: 2021,
+  },
+};
+//name
+//console.log(user.name);
+//или
+//const {name} = user;
+// cover
+//const {book:{cover}} = user;
+// pages
+/*
+const {
+  book: {
+    cover: { pages },
+  },
+} = user100;
+console.log(pages);
+
+console.log(user100.book.cover.pages);
 */
 
-/*
-pow2(2, 5)
-pow2(2, 4)
-pow2(2, 3)
-pow2(2, 2)
-pow2(2, 1) if (exp == 1)   return base=2
-*/
-
-function pow2(base, exp) {
-  if (exp == 1) {
-    return base;
-  }
-  return base * pow2(base, exp - 1);
+//1
+function getUserFullName1(user) {
+  return `${user.name} ${user.surname}`;
 }
 
-console.log(pow2(2, 5));
-
-// Ряд Фибоначчи A[i] = A[i-1] + A[i-2]
-// i:  1 2 3 4 5 6  7  8  9  10 11
-// Ai: 1 1 2 3 5 8 13 21 34  55 89
-
-//return n <= 1 ? n : fib(n - 1) + fib(n - 2)
-
-// сложность n^2
-
-function fibonachi(n) {
-  if (n < 1) {
-    return;
-  }
-  if (n <= 2) {
-    return 1;
-  }
-  return fibonachi(n - 1) + fibonachi(n - 2);
-}
-
-function fibonachi_2(n) {
-  if (n < 1) {
-    return;
-  }
-  if (n <= 2) {
-    return 1;
-  }
-  let fib1 = 1,
-    fib2 = 1;
-  let sum = fib1 + fib2;
-  for (let i = 3; i < n; i++) {
-    fib1 = fib2;
-    fib2 = sum;
-    sum = fib1 + fib2;
-  }
-  return sum;
+//2
+function getUserFullName2(user) {
+  const { name, surname } = user; //!!!!
+  return `${name} ${surname}`;
 }
