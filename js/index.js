956fcede1af8c43e3ad5d466ed4031f32519ee2e
julietvt <freshitpt@gmail.com>
2021-09-11 13:55:21 +0300

    reg exp

--- js/index.js
+++ js/index.js
@@ -1,38 +1,54 @@
-//Создайте карту map для списка пользователей.
-//Каждый пользователь представляет собой отдельный объект со свойствами id, name, age.
-//( В качестве ключа выбрать id)
+//1
+const r1 = /[a-z]+/g;
+//2
+const r2 = new RegExp('\\w+');
 
-users = [
-  { id: 1, name: 'Tom', age: 20 },
-  { id: 2, name: 'John', age: 21 },
-  { id: 3, name: 'Ann', age: 20 },
-  { id: 4, name: 'Tom', age: 25 },
-  { id: 5, name: 'Kate', age: 20 },
-];
+const str = '1234 Tom java script';
 
-/*
-let map = new Map();
-for(let i=0; i<users.length; i++){
-  map.set(users[i].id, {users[i].name, users[i].age});
-}
-*/
-<ul>
-  <li>user1</li>
-  <li>user2</li>
-  <li>user3</li>
-</ul>;
+console.log(r1.test(str));
+console.log(r1.test('hello'));
 
-/*
-<ul>
-  <UserItem> дял user1
-  <UserItem>  для user2
-  <UserItem> для user3
-</ul>;
+// test вернет true/false
+// search / match
 
+console.log(str.search(r1));
 
-mapUser = (user) => {
-  return <UserItem key={user.id} user={user} />;
+console.log(str.match(r1));
+
+let result = str.matchAll(r1);
+console.log(result);
+for (let i of result) {
+  console.log(i);
 }
 
-<ul>{users.map(mapUser)}</ul>
-*/
+function fun1(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i].charCodeAt(0) > '0'.charCodeAt(0) &&
+      str[i].charCodeAt(0) < '9'.charCodeAt(0)
+    ) {
+      continue;
+    }
+    return false;
+  }
+  return true;
+}
+
+function fun2(str) {
+  const reg = /^\d+$/g;
+  return reg.test(str);
+}
+
+const str5 = 'inet 102.168.1.148 netmask 255.255.255.0 broadcast 192.168.1.2';
+const reg = /.+\s(\d{1,3}\.){3}\d{1,3}/;
+/\d{1,3}(?:\.\d{1,3}){3}/;
+
+//Написать функцию, которая очищает строку от всех не буквенно-цифровых символов
+//Пример, "This., -/ is #! a ;: {} to $ % ^ & * book"; преобразуется "This is a book"
+
+let str6 = 'This., -/ is #! a ;: {} to $ % ^ & * book';
+let res = '';
+res = str6.replace(/[^\w\s]/g, '').replace(/\s+/g, ' ');
+
+//Написать функцию для проверки спама в переданной строке. Функция возвращает true, если строка содержит спам. Спамом считать следующие слова: 100% бесплатно, увеличение продаж, только сегодня, не удаляйте, ххх.
+//Функция должна быть нечувствительна к регистру.
