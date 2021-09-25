4239a0771555ecf854368064cbf7e702cfa26245
julietvt <freshitpt@gmail.com>
2021-09-25 13:27:36 +0300

    list. addStart()

--- js/index.js
+++ js/index.js
@@ -1,32 +1,46 @@
-function myArray() {
-  this.length = 0;
-  this.push(...arguments);
+/*
+const ACTION_READ = 'ACTION_READ';
+const ACTION_UPDATE = 'ACTION_READ';
+
+//symbol
+
+const ACTION_READ = Symbol();
+const ACTION_UPDATE = Symbol();
+*/
+
+//итератор
+
+/*
+let a = [1, 2, 3, 4, 5];
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
+*/
+
+//list
+class Item {
+  constructor(value, next = null, prev = null) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
 }
 
-myArray.prototype = {
-  push() {
-    for (let value of arguments) {
-      this[this.length++] = value;
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  addStart(value) {
+    const new_head = new Item(value, this.head, null);
+    if (this.head) {
+      this.head.prev = new_head;
     }
-    return this.length;
-  },
-  pop() {
-    if (this.length > 0) {
-      const result = this[--this.length];
-      delete this[this.length];
-      return result;
+    this.head = new_head;
+    if (!this.tail) {
+      this.tail = new_head;
     }
-  },
-};
-// для обьекта __proto__
-
-let obj1 = {
-  name: 'Tom',
-  surname: 'Fox',
-};
-
-let obj2 = {
-  login: 'loginQwerty123',
-};
-
-obj1.__proto__ = obj2;
+    this.size++;
+  }
+}
