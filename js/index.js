656bef822ac60596766a076e8db36cd8e0a6fd8e
julietvt <freshitpt@gmail.com>
2021-09-25 10:28:55 +0300

    working with object

--- js/index.js
+++ js/index.js
@@ -1,111 +1,66 @@
 /*
-class User {
-  constructor(name, surname, age, isMale = false) {
-    this._name = name;
-    this._surname = surname;
-    this.age = age;
-    this.isMale = isMale;
-  }
-  set name(name) {
-    this._name = name;
-  }
-  get name() {
-    return this._name;
-  }
+const test = {};
 
-  checkAge(value) {
-    if (this.age >= value) return true;
-    return false;
-  }
-  outFullName() {
-    return `${this._name} ${this._surname}`;
-  }
+//1
+test.name = 'test name';
+//2
+test['surname'] = 'some surname';
+
+// числовые свойства
+//ошибка ! test.0='name0';
+test[0] = 'name0';
+
+// вычисляемые свойства 
+
+const userName = 'Tom';
+const userId = 12345;
+const obj1 = {};
+
+obj1[userName + userId] = 'object user1';
+
+console.log('Test object:', test);
+console.log(obj1);
+
+const value = obj1[userName + userId];
+
+console.log('*****************');
+
+for (let i in test) {
+  console.log('obj1.' + i + ' = ' + test[i]);
 }
 
-class Buyer extends User {
-  constructor(name, surname, age, isMale = false) {
-    super(name, surname, age, isMale);
-  }
-  outFullName() {
-    //super.outFullName();
-    //console.log("I'm buyer " + super.outFullName());
-    console.log('Some full name');
-  }
+console.log('*****************');
+
+let arr = [10, 20, 30];
+arr.name = 'hello';
+for (let i in arr) {
+  console.log(i);
 }
-
-class Seller extends User {
-  constructor(name, surname, age, isMale = false) {
-    super(name, surname, age, isMale);
-  }
+console.log('*');
+for (let i of arr) {
+  console.log(i);
 }
-
-const user1 = new User('Ivan', 'Ivanov', 20);
-const user2 = new User('Ann', 'Ivanova', 20, true);
-
-console.log(user1);
-console.log(user1.outFullName());
-console.log(user1.checkAge(18));
-console.log(user2);
-console.log(user2.outFullName());
-console.log(user2.checkAge(18));
-
-const buyer1 = new Buyer('Tom', 'Fox', 20);
-const seller1 = new Seller('John', 'Smith', 20);
-
-console.log(buyer1);
-console.log(buyer1.outFullName());
 */
 
-/*   Задание 1. Реализовать класс RangeValidator со следующими свойствами: 
-■ from (number); 
-■ to (number).
-Реализовать getter'ы и setter'ы для обоих свойств. 
-Реализовать getter range, который будет возвращать массив с двумя числами диапазона. 
-Реализовать метод validate, который будет принимать число и проверять входит ли 
-число в указанный диапазон. 
- */
-
-class RangeValidator {
-  // счетчик экземпляров класса
-  static counter = 0;
-
-  #from = 0;
-  #to = 0;
-  constructor(fromNumber, toNumber) {
-    this.#from = fromNumber;
-    this.#to = toNumber;
-    RangeValidator.counter++;
-  }
-  get to() {
-    return this.#to;
-  }
-  get from() {
-    return this.#from;
-  }
-  set to(toNumber) {
-    this.#to = toNumber;
-  }
-  set from(fromNumber) {
-    this.#from = fromNumber;
-  }
-  get range() {
-    return [this.#from, this.#to];
-  }
-  validate(value) {
-    if (value >= this.#from && value <= this.#to) return true;
-    return false;
-  }
-  static getCounter() {
-    return this.counter;
-  }
+function myArray() {
+  this.length = 0;
 }
 
-const range1 = new RangeValidator(10, 15);
-console.log(range1.from);
-console.log(range1.to);
-//console.log(range1.from(100));
-//console.log(range1.to(120));
-console.log(range1.validate(12));
-console.log(range1.validate(101));
-console.log(range1.validate(150));
-console.log(RangeValidator.getCounter());
+const arrayProto = new myArray();
+
+arrayProto.push = function push() {
+  for (let value of arguments) {
+    this[this.length++] = value;
+  }
+  return this.length;
+};
+
+arrayProto.pop = function pop() {
+  if (this.length > 0) {
+    const result = this[--this.length];
+    delete this[this.length];
+    return result;
+  }
+};
+
+myArray.prototype = arrayProto;
