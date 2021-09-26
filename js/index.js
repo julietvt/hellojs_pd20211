66c87360f0dec0c00414a8ba6eb868ffdb90d2d7
julietvt <freshitpt@gmail.com>
2021-09-26 10:33:09 +0300

    bom. range wrapper

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-class myArray {
-  constructor(iArray) {
-    this._innerArray = iArray;
-  }
-  set innerArray(iArray) {
-    this._innerArray = iArray;
-  }
-  get innerArray() {
-    return this._innerArray;
-  }
-  [Symbol.iterator]() {
-    let index = 0;
-    let arr = this._innerArray;
-    return {
-      next() {
-        return index < arr.length
-          ? { value: arr[index++], done: false }
-          : { done: true };
-      },
-    };
-  }
-}
-
-let array1 = [];
-for (let i = 0; i < 10; i++) {
-  array1.push(Math.floor(Math.random() * 100));
-}
-let objectArray = new myArray(array1);
-
-// 1
-console.log(objectArray.innerArray);
+/*
+const [elem1, elem2] = document.querySelectorAll('h1');
+//1
+//window.addEventListener('resize', fun1);
 //2
-for (let j of objectArray) {
-  console.log(j);
+window.onresize = fun2;
+window.onload = fun2;
+
+
+window.ondblclick = function (e) {
+  console.log(e);
+};
+window.onclick = function (e) {
+  console.log(e);
+};
+window.onkeydown = function (e) {
+  console.log(e);
+};
+
+function fun2(e) {
+  elem1.innerText = `window width = ${window.innerWidth}px`;
+  elem2.innerText = `window heigh = ${window.innerHeight}px`;
 }
+*/
+/*
+function logWindowEvent(e) {
+  console.log(e);
+}
+
+const input = document.querySelector('input[type="text"]');
+input.oninput = logWindowEvent;
+input.onchange = logWindowEvent;
+input.onfocus = logWindowEvent;
+input.onblur = logWindowEvent;
+*/
