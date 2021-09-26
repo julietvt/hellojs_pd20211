515356b81d2c6847981513b2dcad2933f5d88fee
julietvt <freshitpt@gmail.com>
2021-09-26 09:52:19 +0300

    symbol iterator for array

--- js/index.js
+++ js/index.js
@@ -1,14 +1,35 @@
-// генераторы
-//  для обьеявления генератора function *
-// основной метод генератора next()
-// при вызове он возобновляет код до ближайшего yield
-
-function* generator1() {
-  yield 1;
-  yield 2;
-  yield 3;
+class myArray {
+  constructor(iArray) {
+    this._innerArray = iArray;
+  }
+  set innerArray(iArray) {
+    this._innerArray = iArray;
+  }
+  get innerArray() {
+    return this._innerArray;
+  }
+  [Symbol.iterator]() {
+    let index = 0;
+    let arr = this._innerArray;
+    return {
+      next() {
+        return index < arr.length
+          ? { value: arr[index++], done: false }
+          : { done: true };
+      },
+    };
+  }
 }
 
-const r1 = generator1();
+let array1 = [];
+for (let i = 0; i < 10; i++) {
+  array1.push(Math.floor(Math.random() * 100));
+}
+let objectArray = new myArray(array1);
 
-const r2 = generator1();
+// 1
+console.log(objectArray.innerArray);
+//2
+for (let j of objectArray) {
+  console.log(j);
+}
