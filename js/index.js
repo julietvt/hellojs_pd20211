43491068b1e31805be21b2520966c83ed6c9a30e
julietvt <freshitpt@gmail.com>
2021-10-02 14:15:45 +0300

    Awesome slider up-down: fix style buttons and fonts, correct text in color boxes

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const colorSlider = document.querySelector('.slider-color-boxes');
+const imgSlider = document.querySelector('.slider-images');
+const upButton = document.querySelector('.btn-up');
+const downButton = document.querySelector('.btn-down');
+const container = document.querySelector('.container');
+const slidesCount = colorSlider.querySelectorAll('div').length;
