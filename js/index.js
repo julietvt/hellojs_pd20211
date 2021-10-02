e117141602e70482afb2ad6cad6db1d2cc55d678
julietvt <freshitpt@gmail.com>
2021-10-02 10:27:29 +0300

    todo

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
-const btnEl = document.getElementById('btn');
-const listTask = document.getElementById('tasks');
-const inputEl = document.getElementById('task');
+const input = document.querySelector('.input-form');
+const add = document.querySelector('.addButton');
+const del = document.querySelector('.delButton');
+const tasks = document.querySelector('.tasks');
 
-btnEl.addEventListener('click', addTask);
-//let counter = 0;
+add.addEventListener('click', addTask);
+del.addEventListener('click', delTask);
 
-function addTask(e) {
-  const item = document.createElement('li');
-  //item.innerHTML = '<p>Hello' + `${++counter}` + '</p>';
-  item.innerHTML = inputEl.value;
-  listTask.append(item);
+function addTask() {
+  let data = input.value;
+  if (data <= 0) {
+    alert('Nothing entered');
+  } else {
+    let newTask = document.createElement('li');
+    newTask.classList.add('taskElem');
+    newTask.innerHTML = input.value;
+    newTask.append(addCheck());
+    tasks.append(newTask);
+    document.querySelector('.input-form').value = '';
+  }
+}
+
+function addCheck() {
+  const timeElem = document.createElement('div');
+  timeElem.classList.add('time');
+  const time = new Date();
+  timeElem.textContent =
+    time.getFullYear() + '.' + time.getMonth() + '.' + time.getDate();
+  let checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.className = 'checkBox';
+  timeElem.appendChild(checkBox);
+  return timeElem;
+}
+
+function delTask() {
+  const deleteTasks = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  if (deleteTasks) {
+    for (let item of deleteTasks) {
+      item.parentNode.parentNode.remove();
+    }
+  }
 }
