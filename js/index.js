90b6e8d80496071bb4a4c7ae2c2514dc0c96d687
julietvt <freshitpt@gmail.com>
2021-10-03 10:54:29 +0300

    Awesome slider up-down: some fix

--- js/index.js
+++ js/index.js
@@ -34,3 +34,64 @@ function moveToUpDown(dirMove) {
   console.log(colorSlider.style.transform);
   console.log(imgSlider.style.transform);
 }
+
+/*
+function down(){
+    const activeItem = document.querySelector(".active");
+    activeItem.classList.remove("active");
+    index++;
+    imgSlider[index].classList.add("active");
+}
+
+
+buttonDown.addEventListener("click", down);
+
+------
+
+
+.slide{
+  position: absolute;
+  z-index:2;
+}
+.slide.active{
+  transition: outline 0.2s linear;
+  z-index: 15;
+}
+*/
+
+/*
+//3 
+
+arrayBack = [' linear-gradient(220.16deg, #FFE101 -8%, #F39102 138%)',' linear-gradient(220.16deg, #FFE101 -8%, #F39102 138%)']
+
+arrayBack = [[220,100,100],[50,20,100]]
+
+arrayImg = ['some.www/img1.jpg','some.www/img1.jpg','some.www/img1.jpg']
+
+
+<div class="container">
+        <div class="slider-color-boxes">
+                <h2>Snow passion</h2>
+                <p>Love & passion</p>
+        </div>
+        <div class="slider-images">
+        </div>
+        <div class="controlsBtn">
+            <button class="btn-down">
+                <i class="fas fa-arrow-down"></i>
+            </button>
+            <button class="btn-up">
+                <i class="fas fa-arrow-up"></i>
+            </button>
+        </div> 
+    </div>
+    
+  const colorSlider = document.querySelector('.slider-color-boxes'); 
+  const imgSlider = document.querySelector('.slider-images');
+
+  imgSlider.style.background
+
+  arrayBack[index]
+  arrayImg[index]
+
+  */
