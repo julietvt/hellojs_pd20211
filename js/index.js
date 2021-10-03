019af199e3bc155c8b70cd00dfcf1eb8b72fccbd
julietvt <freshitpt@gmail.com>
2021-10-03 10:40:26 +0300

    Awesome slider up-down: add script about move

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
+// 1 slider
 const colorSlider = document.querySelector('.slider-color-boxes');
+const slidesCount = colorSlider.querySelectorAll('div').length;
+// 2 slider
 const imgSlider = document.querySelector('.slider-images');
+// control buttons
 const upButton = document.querySelector('.btn-up');
 const downButton = document.querySelector('.btn-down');
+// container
 const container = document.querySelector('.container');
-const slidesCount = colorSlider.querySelectorAll('div').length;
+// current slide index
+let index = 0;
+
+upButton.addEventListener('click', () => {
+  moveToUpDown('up');
+});
+
+downButton.addEventListener('click', () => {
+  moveToUpDown('down');
+});
+
+function moveToUpDown(dirMove) {
+  if (dirMove == 'up') {
+    index = (index + 1) % slidesCount;
+  } else if (dirMove == 'down') {
+    index = (index - 1 + slidesCount) % slidesCount;
+  } else {
+    console.error('Unknown parameter');
+  }
+  console.log('index = ', index);
+  const offsetHeight = container.clientHeight;
+  colorSlider.style.transform = `translateY(${index * offsetHeight}px)`;
+  imgSlider.style.transform = `translateY(${index * offsetHeight}px)`;
+  console.log(colorSlider.style.transform);
+  console.log(imgSlider.style.transform);
+}
