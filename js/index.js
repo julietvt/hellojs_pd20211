31abb7865d4aa27bf94f8bca12f3583c89605b82
julietvt <freshitpt@gmail.com>
2021-10-09 12:06:20 +0300

    lazy load images

--- js/index.js
+++ js/index.js
@@ -1,35 +1,17 @@
-/* Intersection Observer
-ленивая загрузка изображений видео др контента
-бесконечный скролл
-запуск рекламы , процесса, скрипта 
+//lazy load
 
-let options = {
-  root : // корневой элемент - родитель или viewport (null или не указываем эту опцию )
-  threshold: // порог пересечения 0...1
-  rootMargin: // поля вокруг корневого элемента 
-}
+window.onload = () => {
+  const options = { threshold: 0.5 };
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.src = entry.target.dataset.src;
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
 
-let callbackFun = function(){}
-
-let observer = new IntersectionObserver(callbackFun, options);
-
-let target = document.getElementById('some_target');
-
-observer.observe(target);
-
-*/
-
-const options = { root: null, threshold: 1, rootMargin: '20px' };
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    //console.log('entry:', entry);
-    entry.target.classList.add('active');
-    entry.target.innerHTML = '<p>Block was loaded</p>';
-    observer.unobserve(entry.target);
-  });
-}, options);
-
-const targetCollectionElements = document.querySelectorAll('.back');
-for (let target of targetCollectionElements) {
-  observer.observe(target);
-}
+  document
+    .querySelectorAll('img')
+    .forEach((target) => observer.observe(target));
+};
