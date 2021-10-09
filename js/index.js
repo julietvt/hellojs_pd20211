20e448cb7a0b88f6568adcedb841f663c6efbb37
julietvt <freshitpt@gmail.com>
2021-10-09 12:17:54 +0300

    lazy load video

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-//lazy load
-
-window.onload = () => {
-  const options = { threshold: 0.5 };
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.src = entry.target.dataset.src;
-        observer.unobserve(entry.target);
-      }
-    });
-  }, options);
-
-  document
-    .querySelectorAll('img')
-    .forEach((target) => observer.observe(target));
-};
+if (!!window.IntersectionObserver) {
+  let video = document.querySelector('video');
+  let isPaused = false; /* flag for auto-pausing of the video */
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio != 1 && !video.paused) {
+          video.pause();
+          isPaused = true;
+        } else if (isPaused) {
+          video.play();
+          isPaused = false;
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+  observer.observe(video);
+} else document.querySelector('#warning').style.display = 'block';
