21ac370e9b8a85de16ff832921108650dadbe027
julietvt <freshitpt@gmail.com>
2021-10-09 13:21:47 +0300

    lazy load msg . add script

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
+'use strict';
+const list = document.getElementById('list');
+const listItemLoad = document.getElementById('listItemLoad');
+let counter = 0;
+const observer = new IntersectionObserver(funObserver);
+function funObserver(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      addItems();
+    }
+  });
+}
+observer.observe(listItemLoad);
+
 function addItems() {
   for (let i = 0; i < 20; i++) {
-    FileList.prepend(createListItemLoad());
+    list.prepend(createListItemLoad());
   }
-  FileList.scrollTo(0, 800);
+  list.scrollTo(0, 800);
 }
 
-function createListItemLoad() {}
+function createListItemLoad() {
+  const liElem = document.createElement('li');
+  liElem.classList.add('item');
+  liElem.innerText = `Message ${++counter}`;
+  return liElem;
+}
